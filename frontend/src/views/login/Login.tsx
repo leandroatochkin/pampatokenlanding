f08c89882bc5bc1a0904b55cc920f3a577f8a901
leandroatochkin <leandro.atochkin@outlook.es>
2025-06-25 14:38:35 -0300

    update

--- frontend/src/views/login/Login.tsx
+++ frontend/src/views/login/Login.tsx
@@ -63,7 +63,7 @@ const onSubmit = async (data: LoginData) => {
         });
         const authData = await authRes.json();
         if (authRes.ok) {
-            userStore.getState().setAuthStatus(true, authData.userId)
+            userStore.getState().setAuthStatus(true, authData.userId, authData.userFirstName, authData.userLastName, authData.userEmail, authData.userIsVerified)
             navigate('/operations');
         }
         }
